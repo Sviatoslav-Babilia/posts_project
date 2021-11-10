598f5b76762ccb3c7300e185bc8924de0bccebb4
Sviatoslav Babilia <babilja@gmail.com>
2021-11-10 23:42:19 +0200

    another way select followers posts

--- post_project/posts/views.py
+++ post_project/posts/views.py
@@ -1,43 +1,60 @@
 from django.db import models
-from django.db.models.query import QuerySet
-from django.db.models.query_utils import select_related_descend 
-from django.shortcuts import render
 from .models import User, Post
 from .forms import PostChangeForm
-from django.views.generic import UpdateView, DeleteView, DetailView, TemplateView, FormView, CreateView, ListView
-from django.db.models import OuterRef, Subquery, Prefetch
+from django.views.generic import DetailView, TemplateView, CreateView, ListView
+from django.db.models import Subquery, Prefetch
 
 # Create your views here.
 class UserView(DetailView):
     template_name = 'userpage.html'
+    model = User
+    context_object_name = 'user'
 
-    def get_queryset(self, *args, **kwargs):
-        queryset = Post.objects.all().order_by('title')
-        # import pdb;
-        # pdb.set_trace()
-        return queryset
+    # def get_queryset(self, *args, **kwargs):
+    #     queryset = Post.objects.all().order_by('title')
+    #     # import pdb;
+    #     # pdb.set_trace()
+    #     return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         pk = self.kwargs['pk']
         # following = User.objects.get(pk=pk).followers.all()
-        # followers_posts = Post.objects.filter(author__in=Subquery(following.values('id')))    
+        
+        followers_posts = Post.objects.filter(author__in=Subquery(self.object.followers.all().values('id'))).select_related('author')    
         context['posts'] = Post.objects.all().filter(author=pk).order_by('title')
         # context['followers'] = following
         
-        # voc = {}
+        
         # for us in following:
         #     voc[us.id] = []
 
         # for post in followers_posts:
         #     voc[post.author.id].append(post)
+        # voc = {}
+        # for us in following:
+        #     voc[us.id] = {'user': us, 'posts': 2 }
 
 
-        # context['followersposts'] = voc  
+            
+        following_posts = {}
+        for post in followers_posts:
+            author = post.author
+            if author.id in following_posts:
+                if 'posts' in following_posts[author.id]:
+                    following_posts[author.id]['posts'].append(post)
+                else:
+                    following_posts[author.id]['posts'] = [post]
+            else:
+                    following_posts[author.id] = {'user': author, 'posts': [post]}       
+    
 
-        foll= User.objects.get(pk=pk).followers.all().prefetch_related('followers__posts')
+        context['followersposts'] = following_posts.values()  
 
-        context['foll'] = foll
+        # foll= User.objects.get(pk=pk).followers.all().prefetch_related('followers__posts')
+        
+
+        # context['foll'] = foll
         
         # import pdb;
         # pdb.set_trace()
@@ -56,8 +73,7 @@ class AllPostsView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['posts'] = self.get_queryset()
-        # import pdb;
-        # pdb.set_trace()
+
         return context
     
 
